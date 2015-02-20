540a36fb347372e4aba3fe59a564bc55a9faad82
Marcel Puyat <marcelp@stanford.edu>
2015-02-20 04:43:19 -0800

    Modify algorithm to actually finish running

--- expectation_maximization.py
+++ expectation_maximization.py
@@ -1,40 +1,47 @@
 from itertools import permutations
+from collections import defaultdict
+from stemming.porter2 import stem
+import codecs
+import re
 
 class ExpectationMaximization:
 	def __init__(self, spanishFilename, englishFilename):
 		self.translationProbabilities = dict()
+		self.translationMap = {}
 		self.learnProbabilities(spanishFilename, englishFilename)
+
+	def getTranslationMap(self):
+		return self.translationMap
 
 	def learnProbabilities(self, spanishFilename, englishFilename):
 
 		print("Getting sentences")
 		# This is an array of dictionaries, i.e. [{spanish: ["yo", "soy", ...], english: ["i", "am", ...]}, {spanish:..., english:...}, ...]
-		sentences = self.getSentences(spanishFilename, englishFilename)
+		sentence_pairs = self.getSentencePairs(spanishFilename, englishFilename)
 
 		print("Initializing probability table")
 		# Initialize to have uniform probability for each word to translate to another
-		self.initializeTranslationProbabilityTable(sentences)
-
-		print("Generating alignments")
-		# In the form: [[[0, 1, 2, 3], [0, 1, 2, 3]], [[alignment_1], [alignment_2]], ...]
-		allPossibleAlignments = self.generateAllPossibleAlignments(sentences)
+		all_spanish_words, all_english_words = self.initializeTranslationProbabilityTable(sentence_pairs)
 
 		print("Doing EM")
-		learnUntilConvergence(sentences, allPossibleAlignments)
+		self.learnUntilConvergence(sentence_pairs, all_spanish_words, all_english_words)
 
-	def getSentences(self, spanishFilename, englishFilename):
-		sentences = []
-		spanishSentences = open(spanishFilename).readlines()
-		englishSentences = open(englishFilename).readlines()
+	def getSentencePairs(self, spanishFilename, englishFilename):
+		sentence_pairs = []
+		spanishSentences = codecs.open(spanishFilename, 'r', 'UTF-8').readlines()
+		englishSentences = codecs.open(englishFilename, 'r', 'UTF-8').readlines()
 
 		# assume they are same length
 		for i in range(0, len(spanishSentences)):
-			new_sentence = {}
-			new_sentence['spanish'] = spanishSentences[i].split(' ')
-			new_sentence['english'] = englishSentences[i].split(' ')
-			sentences.append(new_sentence)
+			new_sentence_pair = {}
+			new_sentence_pair['spanish'] = \
+			list(map(lambda word: word.lower(), re.sub("[^\w]|[0-9]", " ", spanishSentences[i], flags=re.UNICODE).split()))
 
-		return sentences
+			new_sentence_pair['english'] = \
+			list(map(lambda word: word.lower(), re.sub("[^\w]|[0-9]", " ", englishSentences[i], flags=re.UNICODE).split()))
+			sentence_pairs.append(new_sentence_pair)
+
+		return sentence_pairs
 
 	def initializeTranslationProbabilityTable(self, sentences):
 		all_spanish_words = set()
@@ -45,107 +52,78 @@ class ExpectationMaximization:
 			for eng_word in sentence["english"]:
 				all_english_words.add(eng_word)
 
-		uniform_prob = 1 / len(all_english_words)
+		uniform_prob = 1 / float(len(all_english_words))
 
 		for spanish_word in all_spanish_words:
-			self.translationProbabilities[spanish_word] = dict()
-			for eng_word in all_english_words:
-				self.translationProbabilities[spanish_word][eng_word] = uniform_prob
+			self.translationProbabilities[spanish_word] = defaultdict(lambda: uniform_prob)
+		return all_spanish_words, all_english_words
 
-	def generateAllPossibleAlignments(self, sentences):
-		# TODO: Can't store permutations like this! Taking too much memory
-		allPossibleAlignments = []
-		for i, sentence in enumerate(sentences):
-			print("Generating alignments for sentence " + str(i))
-			total_num_spanish_words = len(sentence["spanish"])
-			total_num_english_words = len(sentence["english"])
-			print("perms " + str(total_num_spanish_words) + " " + str(total_num_english_words))
-			allPossibleAlignments.append(self.generateAllPermutations(total_num_spanish_words, total_num_english_words))
-
-		return allPossibleAlignments
-
-	def generateAllPermutations(self, total_num_spanish_words, total_num_english_words):
-		return [list(x) for x in permutations(range(total_num_spanish_words), total_num_english_words)]
-
-	def learnUntilConvergence(self, sentences, allPossibleAlignments):
-		oldBestTranslationForEachWord = self.computeBestTranslationForEachWord()
-		while True:
+	def learnUntilConvergence(self, sentence_pairs, all_spanish_words, all_english_words):
+		oldBestTranslationForEachWord = defaultdict(int)
+		for i in range(0,2):
 			print("Iterating")
-			probabilitesForEachAlignment = self.eStep(sentences, allPossibleAlignments)
-			self.mStep(sentences, allPossibleAlignments, probabilitesForEachAlignment)
-			newBestTranslationForEachWord = self.computeBestTranslationForEachWord()
+			counts_of_coocurrences = {}
+			for span_word in all_spanish_words:
+				counts_of_coocurrences[span_word] = defaultdict(int)
+			totals_spanish = defaultdict(int)
 
-			if self.bestTranslationHasConverged(newBestTranslationForEachWord, oldBestTranslationForEachWord):
-				break
+			best_translation_map = {}
+
+			print("Doing all sentences")
+			for idx, sentence_pair in enumerate(sentence_pairs):
+				total_english_words_sent = defaultdict(int)
+				for eng_word in sentence_pair['english']:
+					for spanish_word in sentence_pair['spanish']:
+						total_english_words_sent[eng_word] += self.translationProbabilities[spanish_word][eng_word]
+				for eng_word in sentence_pair['english']:
+					for spanish_word in sentence_pair['spanish']:
+						counts_of_coocurrences[spanish_word][eng_word] += self.translationProbabilities[spanish_word][eng_word] / total_english_words_sent[eng_word]
+						totals_spanish[spanish_word] += self.translationProbabilities[spanish_word][eng_word] / total_english_words_sent[eng_word]
+
+			print("Updating probabilities for each word pair")
+			for span_word in all_spanish_words:
+				best_translation = ""
+				best_prob = 0
+
+				probabilities_for_spanish_word = self.translationProbabilities[span_word]
+				counts_of_span_word_coocurrences = counts_of_coocurrences[span_word]
+				total = totals_spanish[spanish_word]
+				for engl_word in all_english_words:
+					probabilities_for_spanish_word[engl_word] = counts_of_span_word_coocurrences[engl_word] / total
+
+					if probabilities_for_spanish_word[engl_word] > best_prob:
+						best_translation = engl_word
+						best_prob = probabilities_for_spanish_word[engl_word]
+
+				best_translation_map[span_word] = best_translation
+
+			print("Checking for convergence")
+			self.translationMap = best_translation_map
+			if self.bestTranslationHasConverged(best_translation_map, oldBestTranslationForEachWord):
+				self.translationMap = best_translation_map
+				return
 			else:
-				oldBestTranslationForEachWord = newBestTranslationForEachWord
+				oldBestTranslationForEachWord = best_translation_map
 
-	def computeBestTranslationForEachWord(self):
+	def computeBestTranslationForEachWord(self, heaps):
+		print("Computing best translation for each word")
 		bestTranslationForWord = dict()
 		for spanish_word in self.translationProbabilities.keys():
-			bestTranslation = ""
-			bestProbability = 0
-			probabilitiesForSpanishWord = self.translationProbabilities[spanish_word]
-			for eng_word, probability in probabilitiesForSpanishWord.items():
-				if probability > bestProbability:
-					bestProbability = probability
-					bestTranslation = eng_word
-			bestTranslationForWord[spanish_word] = bestTranslation
+			bestTranslationForWord[spanish_word] = heappop(heaps[spanish_word])[1]
 		return bestTranslationForWord
 
-	def eStep(self, sentences, allPossibleAlignments):
-		probabilitesForEachAlignment = []
-		for i, sentence in enumerate(sentences):
-			probabilitiesForSentence = []
-			probabilitesForEachAlignment.append(probabilities)
-			alignmentsForSentence = allPossibleAlignments[i]
-
-			for alignment in alignmentsForSentence:
-				probabilityForAlignment = 1
-				sumOfProbabilitiesForSentence = 0
-
-				for word_idx, spanish_word in enumerate(sentence['spanish']):
-					probabilityForAlignment *= self.translationProbabilities[spanish_word][sentence['english'][alignment[word_idx]]]
-
-				sumOfProbabilitiesForSentence += probabilityForAlignment
-				probabilitiesForSentence.append(probabilityForAlignment)
-
-				# Normalize
-				for i in range(0, len(probabilitiesForSentence)):
-					probabilitiesForSentence[i] /= sumOfProbabilitiesForSentence
-
-			probabilitesForEachAlignment.append(probabilitiesForSentence)
-
-		return probabilitesForEachAlignment
-				
-
-	def mStep(self, sentences, allPossibleAlignments, probabilitesForEachAlignment):
-		# First zero out all probabilities
-		for spanish_word in self.translationProbabilities:
-			for eng_word in self.translationProbabilities[spanish_word]:
-				self.translationProbabilities[spanish_word][eng_word] = 0
-
-		# For each translated word pair, add the probability of the alignment to the table
-		for sentence_idx, sentence in enumerate(sentences):
-			for alignment_idx, alignment in enumerate(allPossibleAlignments[sentence_idx]):
-				for word_idx, spanish_word in enumerate(sentence['spanish']):
-					english_word_aligned_to_this = sentence['english'][alignment[word_idx]]
-					self.translationProbabilities[spanish_word][english_word_aligned_to_this] += probabilitesForEachAlignment[sentence_idx][alignment_idx]
-
-		# Normalize probabilities for each word
-		for spanish_word in self.translationProbabilities:
-			total_sum = 0
-			for eng_word in self.translationProbabilities[spanish_word]:
-				total_sum += self.translationProbabilities[spanish_word][eng_word]
-
-			for eng_word in self.translationProbabilities[spanish_word]:
-				self.translationProbabilities[spanish_word][eng_word] /= total_sum
-
-	def bestTranslationHasConverged(newBestTranslationForEachWord, oldBestTranslationForEachWord):
+	def bestTranslationHasConverged(self, newBestTranslationForEachWord, oldBestTranslationForEachWord):
+		print("Comparing old best translation to new")
 		for spanish_word in newBestTranslationForEachWord:
-			if newBestTranslationForEachWord[spanish_word] is not oldBestTranslationForEachWord[spanish_word]:
+			if newBestTranslationForEachWord[spanish_word] != oldBestTranslationForEachWord[spanish_word]:
 				return False
 		return True
 
 if __name__ == '__main__':
 	x = ExpectationMaximization('es-en/dev/newstest2012.es', 'es-en/dev/newstest2012.en')
+	print("Writing to file")
+	f = codecs.open("overnight_output2", "w", "UTF-8")
+	for spanish_word, eng_word in x.getTranslationMap().items():
+		f.write(spanish_word + " :: " + eng_word + "\n")
+	f.close()
+
