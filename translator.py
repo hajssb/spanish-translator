80ad502f2b77c589e31caa8936cda09549e1e627
Marcel Puyat <marcelp@stanford.edu>
2015-02-25 00:14:04 -0800

    few more improvements. BLEU-2 = 10.83

--- translator.py
+++ translator.py
@@ -17,6 +17,9 @@ def should_swap(tag, nextTag, firstWord, nextWord):
 	(tag == "RB" and nextTag == "VBZ") or \
 	(firstWord == "states" and nextWord == "united")
 
+def is_verb(tag):
+	return "VB" in tag
+
 def rearrange_pos(english_words):
 	tags = [x[1] for x in pos_tag(english_words)]
 	i = 0
@@ -35,16 +38,11 @@ def convert_escaped_chars(word):
 		return "&apos;"
 	return word
 
-def should_remove(word):
+def is_reverse_punctuation(word):
 	return word.decode('utf-8') == u'\u00bf' or word.decode('utf-8') == u'\u00A1'
 
 def is_vowel(char):
 	return char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u'
-
-def replace_a_and(english_words):
-	for i in range(0, len(english_words)-1):
-		if english_words[i] == "a" and is_vowel(english_words[i+1][0]):
-			english_words[i] = "an"
 
 def delete_repeated_words(english_words):
 	ret = []
@@ -53,24 +51,53 @@ def delete_repeated_words(english_words):
 			ret.append(word)
 	return ret
 
-# def remove_a_before_percent(english_words):
-# 	ret = []
-# 	for i in range(0, len(english_words)-1):
-# 		if len(ret) == 0: ret.append(english_words[i])
-# 		else:
-# 			prev_word = english_words[i-1]
-# 			word = english_words[i]
-# 			next_word = english_words[i+1]
-# 			if (prev_word == 'a' or prev_word == 'the') and word.isdigit() and next_word == "%":
-# 				continue
-# 			else:
-# 				ret.append(word)
-# 	return ret
+def replace_a_with_an(sentence):
+	return re.sub("(^| )a ([aeiou]\w+)", lambda m: m.group(1) + "an " + m.group(2), sentence)
+
+def remove_a_before_percent(sentence):
+	return re.sub("(^| )a ([0-9]+ %)", lambda m: m.group(1) + m.group(2), sentence)
+
+def remove_the_before_has(sentence):
+	return re.sub("(^| )the has ", lambda m: m.group(1) + "has ", sentence)
+
+def replace_not_i(sentence):
+	return re.sub("(^| )not i ", lambda m:m.group(1) + "i don &apos;t ", sentence)
+
+def replace_regex(english_words):
+	sentence = " ".join(english_words)
+
+	sentence = replace_a_with_an(sentence)
+	sentence = remove_a_before_percent(sentence)
+	sentence = remove_the_before_has(sentence)
+	sentence = replace_not_i(sentence)
+
+	return sentence.split()
+
+def improvements(english_words):
+	english_words = rearrange_pos(english_words)
+	english_words = replace_regex(english_words)
+	english_words = delete_repeated_words(english_words)
+	return english_words
+
+def infinitive_verbs(span_sent):
+	return re.sub("(^| )([a-zA-Z]+ar)", lambda m: m.group(1) + "a " + m.group(2), span_sent)
+
+def regex_improvements(spanish_sentence):
+	as_str = " ".join(spanish_sentence)
+	# as_str = infinitive_verbs(as_str)
+	return as_str.split()
+
+def improvements_spanish(spanish_sentence):
+	spanish_sentence = regex_improvements(spanish_sentence)
+	return spanish_sentence
 
 def translate_sentence(spanish_sentence):
 	english_words = []
+
+	spanish_sentence = improvements_spanish(spanish_sentence)
+
 	for spanish_word in spanish_sentence:
-		if should_remove(spanish_word):
+		if is_reverse_punctuation(spanish_word):
 			continue
 
 		translated_word = spanish_word
@@ -80,10 +107,7 @@ def translate_sentence(spanish_sentence):
 		translated_word = convert_escaped_chars(translated_word)
 		english_words.append(translated_word)
 
-	english_words = rearrange_pos(english_words)
-	replace_a_and(english_words)
-	english_words = delete_repeated_words(english_words)
-	# english_words = remove_a_before_percent(english_words)
+	english_words = improvements(english_words)
 	return english_words
 
 if __name__ == '__main__':
